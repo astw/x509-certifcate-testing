9971884c1c6bfd05ae29dd097b7e73ea0493df74
WATERTRAX\swang <swang@watertrax.com>
2017-05-31 08:56:18 -0700

    test filters

---
+++ TestMVC5/Handler/MyHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC5.Handler
+{
+    public class MyHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("The page request is " + context.Request.RawUrl.ToString());
+            StreamWriter sw = new StreamWriter(@"C:\requestLog.txt", true);
+            sw.WriteLine("Page requested at " + DateTime.Now.ToString() +
+                        context.Request.RawUrl); sw.Close();
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ TestMVC5/Module/MyModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC5
+{
+    public class MyModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(this.Context_BeginRequest);
+            context.EndRequest += new EventHandler(this.Context_EndRequest);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Context_EndRequest(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\requestLog.txt", true);
+            sw.WriteLine("End Request called at " + DateTime.Now.ToString()); sw.Close();
+        }
+        public void Context_BeginRequest(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\requestLog.txt", true);
+            sw.WriteLine("Begin request called at " + DateTime.Now.ToString()); sw.Close();
+        }
+    }
+}
